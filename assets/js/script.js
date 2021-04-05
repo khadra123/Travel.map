bf4b36ed585d2ef83e102b6e2650f9a306e644fb
Khadra Mohamed <khadra.m123@gmail.com>
2021-04-05 13:19:23 -0400

    added icons and some html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // set variables 
-
-var searchHistoryArray = [];
 var search = document.querySelector('#search');
 var searchBtn = document.querySelector('#countrySearchBtn');
 var searchHistoryBtn = document.querySelector('#searchHistoryBtn');
 var cityDisplayCard = document.querySelector('#cityCard');
 var displayImageCard = document.querySelector('#imageCard');
-var returnedCityResults = [];
 var searchHistoryCard = document.querySelector('#historyCard');
+
+//
+var searchHistoryArray = [];
+var returnedCityResults = [];
