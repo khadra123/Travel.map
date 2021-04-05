45efbb4f7587c230ac544dd4b19d289f62f0700a
chaitalizn <phchaitali3@gmail.com>
2021-04-04 23:23:58 -0400

    api fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,26 @@ var displayImageCard = document.querySelector('#imageCard');
 var returnedCityResults = [];
 var searchHistoryCard = document.querySelector('#historyCard');
 
-var countries = "US"
+var countries = 'canada'
 
-fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/countries/" + countries + "/regions", {
+fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/countries/" + "CA" + "/regions", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "edc80740bfmsh7af0ea3522b23e7p1413b9jsnbec35903712b",
+		"x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
+	}
+})
+.then(function(response) {
+  return response.json();
+})
+.then(function(response) {
+  console.log(response)
+})
+.catch(function(err) {
+	console.error(err);
+});
+
+fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/countries?limit=1&offset=0&namePrefix=" + countries, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "9898e76638msh7fc219f72ab87d6p18d7ddjsn2c6c05aff44d",
