c5d15747cfbcfe1615060d96fb09209cc187b845
chaitalizn <phchaitali3@gmail.com>
2021-04-06 00:16:39 -0400

    added function to save searchs to local storage and in recent search section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,21 @@ var returnedCityResults = [];
 var searchHistoryCard = document.querySelector('#historyCard');
 
 var searchHistoryCard = document.querySelector('#historyCard');
+// storing user input country in this var after onclick event
+var userSearchCountry;
 var countryCode;
 var countryCodeTwo;
 //
 var searchHistoryArray = [];
 var returnedCityResults = [];
 
+//store country search to local storage 
+if (!localStorage.getItem('recentSearch')) {   
+	localStorage.setItem('recentSearch', JSON.stringify(searchHistoryArray));
+	}
+	var historySearchList = JSON.parse(localStorage.getItem('recentSearch'));  
 
 
-// storing user input country in this var
-var userSearchCountry;
 
 //start of all functions 
 
@@ -37,6 +42,26 @@ function countrySearchFunction(){
 
 		//imageFunction()
 
+		//add new search to local storage
+
+		//display recent searchs in navbar
+		var recentSearchNavBtn = $('<a>').html('class="nav-link" href="#recent-search-container" Recent Searchs');
+		recentSearchNavBtn.attr('href', '#recent-search-container');
+		recentSearchNavBtn.addClass('nav-link');
+		recentSearchNavBtn.text('Recent Searchs');
+		$('.navbar-nav').append(recentSearchNavBtn);
+
+		//display recent section on html
+		$('#recent-search-btn').css('display', 'block');
+		$('#recent-search-container').css('display', 'block');
+
+
+		if (historySearchList) {
+			historySearchList.push(userSearchCountry);
+			localStorage.setItem('recentSearch', JSON.stringify(historySearchList));
+		}
+		
+
 	} else {
 		//display error msg on html
 		var incorrectInputMsg = $('<p>');
@@ -45,8 +70,23 @@ function countrySearchFunction(){
 		$("#searchForm").append(incorrectInputMsg);
 	
 	}
+	recentSearchBtn()
 }
 
+	// function to get recent search data from local storage and create buttons
+	function recentSearchBtn() {
+    
+		$('#past-search-btn').html('');
+	
+		for (i = 0; i < historySearchList.length; i++) {
+			var btn = $('<button>');
+			btn.addClass('btn btn-info m-2 my-sm-0');
+			btn.attr('type', 'button');
+			btn.text(historySearchList[i]);
+			btn.attr('data-city', historySearchList[i]);
+			$('#recent-search-btn').append(btn);
+		}
+	}
 
 	//api for image ----I have commented out your callback in countrySearchFunction (line 33) for testing purpose -chaitali
 	function imageFunction () {
